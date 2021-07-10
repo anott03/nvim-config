d276ca384d032f20f5fff52355f5b206b46eae09
anott03 <amitav_nott@ryecountryday.org>
2021-07-10 12:14:39 -0400

    getting notifications to work as a queue

--- lua/a/dev/notification.lua
+++ lua/a/dev/notification.lua
@@ -1,5 +1,5 @@
 local api = vim.api
-local Job = require('plenary.job')
+local popup = require('popup')
 
 local function location_window(options)
     local default_options = {
@@ -34,44 +34,53 @@ end
 
 -- ACTIVE_NOTIFICATIONS = ACTIVE_NOTIFICATIONS or {}
 
-local function create_notification(text)
+local notification_config = {
+  icon = ""
+}
+
+local function create_notification(sender, text)
   local win_stats = api.nvim_list_uis()[1]
   local win_width = win_stats.width
 
   local prev_win = api.nvim_get_current_win()
 
+  -- TODO: position probably shouldn't done this way
   local info = location_window({
       width = 20,
       height = 2,
       row = 1,
-      col = win_width - 21,
+      col = win_width - 25,
   })
 
   api.nvim_buf_set_lines(
-      info.bufnr, 0, 5, false, { "!!! Notification", text }
+      info.bufnr, 0, 5, false, { string.format("%s %s", notification_config.icon, sender), text }
   )
   api.nvim_set_current_win(prev_win)
+
   return {
-    info.bufnr,
-    info.win_id
+    bufnr = info.bufnr,
+    win_id = info.win_id
   }
 end
 
 NOTIFICAION_QUEUE = {}
 Notification = {}
 
-function Notification:new(text, lifetime)
-  self.text = text
-  self.lifetime = lifetime
+function Notification:new(sender, text, lifetime)
+  local obj = {}
+
+  obj.sender = sender
+  obj.message = text
+  obj.lifetime = lifetime
+  -- table.insert(NOTIFICAION_QUEUE, obj)
+  return setmetatable(obj, Notification)
 end
 
 function Notification:render()
-  create_notification(self.text)
+  local info = create_notification(self.sender, self.message)
+  self.bufnr = info.bufnr
 
-  -- vim.defer_fn(CLOSE_NOTIFICATION, self.lifetime or 3000)
-  vim.defer_fn(function()
-    self.close()
-  end, self.lifetime or 3000)
+  vim.defer_fn(CLOSE_NOTIFICATION, self.lifetime or 3000)
 end
 
 function Notification:close()
@@ -80,12 +89,43 @@ function Notification:close()
     return
   end
 
-  api.nvim_buf_delete(self.bufnr)
+  api.nvim_buf_delete(self.bufnr, { force = true })
   self.bufnr = nil
 end
 
+-- TODO: interface to manage notifications (close, delete, reorder, etc.)
+function NOTIFICATIONS()
+  local bufnr = api.nvim_create_buf(false, false)
+  local width = 60
+  local height = 10
+  local win_id, win = popup.create(bufnr, {
+      title = "Messages",
+      highlight = "Normal",
+      line = math.floor(((vim.o.lines - height) / 2) - 1),
+      col = math.floor((vim.o.columns - width) / 2),
+      minwidth = width,
+      minheight = height,
+      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+  })
 
+  vim.api.nvim_win_set_option(
+      win.border.win_id,
+      "winhl",
+      "Normal:Normal"
+  )
 
+  local lines = {}
+  for _, notification in pairs(NOTIFICAION_QUEUE) do
+    table.insert(lines, string.format("%s :: %s", notification.sender, notification.message))
+  end
+
+  api.nvim_buf_set_lines(bufnr, 0, #lines, false, lines)
+
+  return {
+    win_id,
+    win
+  }
+end
 
 function CLOSE_NOTIFICATION()
   if #NOTIFICAION_QUEUE == 0 then
@@ -93,6 +133,17 @@ function CLOSE_NOTIFICATION()
     return -1
   end
 
-  local current_notification = NOTIFICAION_QUEUE[#NOTIFICAION_QUEUE]
-  api.nvim_buf_delete(current_notification[1], { force = true })
+  local current_notification = NOTIFICAION_QUEUE[1]
+  current_notification:close()
+
+  table.remove(NOTIFICAION_QUEUE, 1)
+  if #NOTIFICAION_QUEUE ~= 0 then
+    NOTIFICAION_QUEUE[1]:render()
+  end
 end
+
+Notification.__index = Notification
+
+table.insert(NOTIFICAION_QUEUE, Notification:new('another', 'notification', 3000))
+table.insert(NOTIFICAION_QUEUE, Notification:new('test', 'hello', 3000))
+NOTIFICAION_QUEUE[1]:render()
