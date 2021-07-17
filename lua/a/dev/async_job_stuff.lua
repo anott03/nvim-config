4eec73078394c3ecc742e8919cca5c73d962f5e9
anott03 <amitav_nott@ryecountryday.org>
2021-07-17 19:28:08 -0400

    async job stuff

--- lua/a/dev/async_job_stuff.lua
+++ lua/a/dev/async_job_stuff.lua
@@ -1,6 +1,8 @@
-local async = require('plenary.async_lib')
+local async_lib = require('plenary.async_lib')
+local async = async_lib.async
+local await = async_lib.await
 
-local f = async.wrap(function(params)
+local f = async_lib.wrap(function(params)
   return params
 end, 'vararg')
 
@@ -8,12 +10,11 @@ end, 'vararg')
   -- f()(3)
 -- ))
 
-local g = async.async(function(params)
+local g = async(function(params)
   print(vim.inspect(params))
 end)
 
-local future = g()
-
-async.run(future, function()
-  print('done')
-end)
+-- async.run(future, function()
+  -- print('done')
+-- end)
+await(g(3))
